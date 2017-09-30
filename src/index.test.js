bd1bde7e380a2447690cbefae3699f1b81b25ef4
Federico Pinna <fpinna@vivocha.com>
2017-09-30 10:05:52 +0200

    feat(package): Updated to latest fseh

--- src/index.test.js
+++ src/index.test.js
@@ -8,17 +8,17 @@ chai.use(sinonChai);
 
 describe('fsehx', function() {
 
-  describe('enter', function() {
-    it('should emit events when entering a state', function() {
-      var m = new MachineX({
+  describe('enter', async function() {
+    it('should emit events when entering a state', async function() {
+      const m = new MachineX({
         start: {}
       });
 
-      var named_pre_entry = sinon.spy();
-      var pre_entry = sinon.spy();
-      var named_entry = sinon.spy();
-      var entry = sinon.spy();
-      var named = sinon.spy();
+      let named_pre_entry = sinon.spy();
+      let pre_entry = sinon.spy();
+      let named_entry = sinon.spy();
+      let entry = sinon.spy();
+      let named = sinon.spy();
 
       m.on('start:pre-entry', named_pre_entry);
       m.on('pre-entry', pre_entry);
@@ -27,39 +27,36 @@ describe('fsehx', function() {
       m.on('start', named);
 
       should.not.exist(m.state);
-      return m.enter('start', 'aaa').then(function() {
-        should.exist(m.state);
-        m.state.should.be.a('string');
-        m.state.should.equal('start');
-        named_pre_entry.should.have.been.calledOnce;
-        pre_entry.should.have.been.calledOnce;
-        pre_entry.should.have.been.calledWith('start', 'aaa');
-        named_entry.should.have.been.calledOnce;
-        entry.should.have.been.calledOnce;
-        entry.should.have.been.calledWith('start', 'aaa');
-        named.should.have.been.calledOnce;
-      });
+      await m.enter('start', 'aaa');
+      should.exist(m.state);
+      m.state.should.be.a('string');
+      m.state.should.equal('start');
+      named_pre_entry.should.have.been.calledOnce;
+      pre_entry.should.have.been.calledOnce;
+      pre_entry.should.have.been.calledWith('start', 'aaa');
+      named_entry.should.have.been.calledOnce;
+      entry.should.have.been.calledOnce;
+      entry.should.have.been.calledWith('start', 'aaa');
+      named.should.have.been.calledOnce;
     });
 
-    it('should emit events when exiting a state', function() {
-      var m = new MachineX({
+    it('should emit events when exiting a state', async function() {
+      const m = new MachineX({
         start: {},
         end: {}
       }, 'start');
 
-      var named_exit = sinon.spy();
-      var exit = sinon.spy();
+      let named_exit = sinon.spy();
+      let exit = sinon.spy();
 
       m.on('start:exit', named_exit);
       m.on('exit', exit);
 
-      m.ready.then(function() {
-        return m.enter('end', 'aaa').then(function() {
-          named_exit.should.have.been.calledOnce;
-          exit.should.have.been.calledOnce;
-          exit.should.have.been.calledWith('start');
-        });
-      });
+      await m.ready;
+      await m.enter('end', 'aaa');
+      named_exit.should.have.been.calledOnce;
+      exit.should.have.been.calledOnce;
+      exit.should.have.been.calledWith('start');
     });
 
   });
